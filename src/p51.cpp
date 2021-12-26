7d44cd85ddf1240d93d1bb7beeb725aff0302314
John Mastroberti <johnmastroberti123@gmail.com>
2021-12-26 14:14:43 -0500

    p51 completed

--- src/p51.cpp
+++ src/p51.cpp
@@ -5,7 +5,7 @@
 #include "primes.hpp"
 
 char common_3_digits(const std::string& p) {
-  auto has_3_of = [&p](char c) { return std::ranges::count(p, c) >= 3; };
+  auto has_3_of = [&p](char c) { return std::ranges::count(p, c) == 3; };
   auto it = std::ranges::find_if(p, has_3_of);
   if (it == p.end())
     return '\0';
@@ -15,43 +15,32 @@ char common_3_digits(const std::string& p) {
 
 bool has_prop_p51(std::string p) {
   auto d = common_3_digits(p);
+  if (p == "56003") spdlog::info("For 56003, d = '{}'", d);
   if (d == '\0') return false;
 
-  static u64 counter = 0;
-  counter++;
-  if (counter % (1 << 10) == 0) print(p);
+  auto it1 = std::ranges::find(p, d);
+  auto it2 = std::ranges::find(it1 + 1, p.end(), d);
+  auto it3 = std::ranges::find(it2 + 1, p.end(), d);
   constexpr std::span digits("0123456789", 10);
-  return std::ranges::count_if(digits, [&p, d](char c) {
-           std::ranges::replace(p, d, c);
-           return is_prime(std::stoull(p));
-         }) == 8;
+  auto dstart = (it1 == p.begin()) ? digits.begin() + 1 : digits.begin();
+  auto count =
+      std::ranges::count_if(dstart, digits.end(), [&p, it1, it2, it3](char c) {
+        *it1 = c;
+        *it2 = c;
+        *it3 = c;
+        return is_prime(std::stoull(p));
+      });
+  return count == 8;
 }
 
 void p51() {
   Primes<u64> primes;
 
-  // auto status = [max = 0ull](u64 p) mutable {
-  //   const u64 chunk_size = 10'000;
-  //   if (p / chunk_size > max) {
-  //     print(p);
-  //     max++;
-  //   }
-  // };
-
   for (;;) {
     auto p = primes.next();
-    // status(p);
     if (has_prop_p51(std::to_string(p))) {
       print_answer(51, p);
       return;
     }
   }
-  // std::vector<std::string> pstrings;
-  // std::ranges::transform(primes.up_to(1'000'000), std::back_inserter(pstrings),
-  //                        [](auto x) { return std::to_string(x); });
-  // auto begin = std::ranges::find_if(
-  //     pstrings, [](auto const& s) { return s.size() == 5; });
-  // std::copy_if(begin, pstrings.end(),
-  //              std::ostream_iterator<std::string>(std::cout, "\n"),
-  //              has_prop_p51);
 }
